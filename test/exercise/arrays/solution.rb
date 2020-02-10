146526be0dd85f787f6ceec5fea5f2ae19817ec3
GabitovArtur <coolermast@gmail.com>
2020-02-10 16:28:58 +0500

    delete lambda

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,35 +2,39 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array[0]
-        search_max = lambda do |index|
-          return if index == array.length - 1
-          max = array[index] if array[index] > max
-          return search_max.call(index + 1)
-        end
-        search_max.call(0)
+        max = max(array)
         array.map { |el| (el < 0) ? el : max }
       end
 
-      def search(array, query)
-        return -1 if array.empty?
-        binary_search = lambda do |first_index, last_index|
-          if first_index == last_index
-            return query == array[last_index] ? last_index : -1
-          end
-          middle_index = (first_index + last_index) / 2
-          middle_element = array[middle_index]
-          check = middle_element - query
-          case check
-          when ->(x) { x == 0 }
-            return middle_index
-          when ->(x) { x < 0 }
-            binary_search.call(middle_index + 1, last_index)
-          when ->(x) { x > 0 }
-            binary_search.call(first_index, middle_index - 1)
-          end
+      def max(array)
+        max = array.last
+        index = 1
+        while index < array.length
+          max = array[index] if array[index] > max
+          index += 1
         end
-        binary_search.call(0, array.length - 1)
+        max
+      end
+
+      def search(array, query)
+        query = Hash[:first, 0, :last, array.length - 1, :searching_element, query] unless query.class == Hash
+        return -1 if array.empty?
+        if query[:first] == query[:last]
+          return query[:searching_element] == array[query[:last]] ? query[:last] : -1
+        end
+        middle_index = (query[:first] + query[:last]) / 2
+        middle_element = array[middle_index]
+        check = middle_element - query[:searching_element]
+        case check
+        when ->(x) { x == 0 }
+          middle_index
+        when ->(x) { x < 0 }
+          query[:first] = middle_index + 1
+          search(array, query)
+        when ->(x) { x > 0 }
+          query[:last] = middle_index - 1
+          search(array, query)
+        end
       end
     end
   end
