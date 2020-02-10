a70df3d73bff72e628dc3bd31bfbce76bc5b5dbb
GabitovArtur <coolermast@gmail.com>
2020-02-10 18:09:26 +0500

    Revert "refactor binary search"
    This reverts commit 2160cf2e340439cee4eed3031fa25068c21f7d71.

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,21 +4,12 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(films)
-        matched_films = films.select do |film|
-          film['country'].to_s.include?(',') && film['rating_kinopoisk'] &&
-            film['rating_kinopoisk'].to_f.positive?
-        end
-        ratings = matched_films.map { |film| film['rating_kinopoisk'].to_f }
-        count = ratings.length
-        total = ratings.reduce(:+)
-        total / count
+      def rating(_array)
+        0
       end
 
-      def chars_count(films, threshold)
-        searching_letter = 'и'
-        filtered_films = films.select { |film| film['rating_kinopoisk'].to_f > threshold }
-        filtered_films.map { |film| film['name'].to_s.count(searching_letter) }.reduce(:+)
+      def chars_count(_films, _threshold)
+        0
       end
     end
   end
