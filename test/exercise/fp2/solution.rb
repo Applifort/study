62587b19900ebb0c535f6b2b9ca15e5a7241637b
GabitovArtur <coolermast@gmail.com>
2020-02-13 10:40:33 +0500

    Add my primitive functions

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,32 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        index = 0
+        while index < length
+          yield self[index]
+          index += 1
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new
+        my_each { |element| result << yield(element) }
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        result = MyArray.new
+        my_each { |element| result << element unless element.nil? }
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        my_each { |element| acc = acc.nil? ? element : yield(acc, element) }
+        acc
       end
     end
   end
