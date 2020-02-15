c3145e1c631ebbb7075d2fcde23ee756b5561fab
GabitovArtur <coolermast@gmail.com>
2020-02-15 20:29:55 +0500

    refactor some name of var

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -3,12 +3,13 @@ module Inatra
     @@routes = {}
 
     def routes(&block)
-      raise 'No ruote given' unless block_given?
+      raise 'No route given' unless block_given?
       instance_eval(&block)
     end
 
-    def method_missing(name, path)
-      @@routes[name.to_s.upcase] = { path => yield }
+    def method_missing(verb, path)
+      raise 'What to do?' unless block_given?
+      @@routes[verb.to_s.upcase] = { path => yield }
     end
 
     def call(env)
