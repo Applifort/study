9fcba49cdc592e94e36e6569a654c86c3ce85029
GabitovArtur <coolermast@gmail.com>
2020-02-15 20:24:50 +0500

    I create framework

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,18 @@
 module Inatra
   class << self
-    def routes
+    @@routes = {}
+
+    def routes(&block)
+      raise 'No ruote given' unless block_given?
+      instance_eval(&block)
+    end
+
+    def method_missing(name, path)
+      @@routes[name.to_s.upcase] = { path => yield }
     end
 
     def call(env)
+      @@routes[env['REQUEST_METHOD']][env['PATH_INFO']]
     end
   end
 end
